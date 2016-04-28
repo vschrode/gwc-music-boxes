204b26a0df0986b270275c48f59463010d423aaa
Jasmine Kim <jasminekim95@yahoo.com>
2016-04-28 15:55:43 -0700

    updated

--- static/js/index.js
+++ static/js/index.js
@@ -4,7 +4,8 @@ $(document).ready( function() {
     // This controls the button for the c note.
     
     // This variable calls the cNote audio element in the HTML.
-    
+    var cNote = document.getElementById('cAudio');
+
     // This turns our <div id= "c"> into a button that plays our audio file when the box is pressed.
     $('#c').mousedown(function(){
         
